a823dd5d6a8ba0f29e0ea2b0aceb4bd332c1a84f
hojun.yoon <tjdsksro@gmail.com>
2023-11-21 22:58:36 +0900

    메인 틀 작업, react-icons 모듈 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import * as Styled from "../assets/BasicStyle";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Styled.FooterBgStyle>
+      <Styled.FooterStyle>copyright @B-8</Styled.FooterStyle>
+    </Styled.FooterBgStyle>
+  );
 }
 
 export default Footer;
