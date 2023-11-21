6cf4468e8da3fdac970766cd4e7c86863cff9803
hojun.yoon <tjdsksro@gmail.com>
2023-11-21 22:58:36 +0900

    메인 틀 작업, react-icons 모듈 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,36 @@
+import * as Styled from "assets/BasicStyle";
+import * as MainStyled from "assets/main/index";
 import { app } from "firbase";
 import React, { useEffect } from "react";
+import { AiFillCheckCircle } from "react-icons/ai";
 
 function Main() {
   useEffect(() => {
     console.log("app", app);
   }, []);
-  return <div>Main</div>;
+  return (
+    <MainStyled.BoxWrapBg>
+      <MainStyled.BoxWrap>
+        <MainStyled.BoxLeft>left : 로그인 정보 및 카테고리?</MainStyled.BoxLeft>
+        <MainStyled.BoxRight>
+          <Styled.BoxBtn line="line" width="100%">
+            확인용 박스
+          </Styled.BoxBtn>
+          <Styled.BoxBtn>확인용 박스</Styled.BoxBtn>
+          <Styled.FlexCenter>
+            <Styled.RountImg>
+              <Styled.ImgWidth src="" alt="" />
+            </Styled.RountImg>
+            <span>
+              <span>오늘 할 이야기가 뭔가요?</span>
+              <AiFillCheckCircle />
+            </span>
+          </Styled.FlexCenter>
+          <Styled.Line></Styled.Line>
+        </MainStyled.BoxRight>
+      </MainStyled.BoxWrap>
+    </MainStyled.BoxWrapBg>
+  );
 }
 
 export default Main;
