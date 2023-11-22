d24c72e8684779c21809f471519f3803b96f95ab
eiansd <joh7727@naver,com>
2023-11-22 21:11:51 +0900

    첫날 작업 푸쉬

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 // import "assets/reset.css";
+import { auth } from "./firebase";
+import { useEffect } from "react";
 import Router from "shared/Router";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function App() {
+
   return <Router />;
 }
 
